7121edc3ef015ad2b856d8eef4fa9c34ce744799
Max Ott <max.ott@nicta.com.au>
2015-02-10 15:04:12 +1100

    Now also being able to read CSV from a string

--- lib/omf_oml/csv_table.rb
+++ lib/omf_oml/csv_table.rb
@@ -19,10 +19,10 @@ module OMF::OML
 
     # @param opts
     #
-    def self.create(tname, file_name, opts = {}, &on_before_row_added)
-      opts[:file_name] = file_name
-      self.new(tname, opts[:schema], opts, &on_before_row_added)
-    end
+    # def self.create(tname, file_name, opts = {}, &on_before_row_added)
+    #   opts[:file_name] = file_name
+    #   self.new(tname, opts[:schema], opts, &on_before_row_added)
+    # end
 
     #
     # tname - Name of table
@@ -30,45 +30,56 @@ module OMF::OML
     #   Table adds a '__id__' column at the beginning which keeps track of the rows unique id
     # @params opts
     # @opts :file_name - Name of file to read data from
+    # @opts :text - Text to parse
     # @opts :max_size - keep table to that size by dropping older rows
     # @opts :index - only keep the latest inserted row for a unique col value - messes with row order
     # @opts :has_csv_header If true use first row in file as schema descriptor
     # @opts :schema Schema associated with this table
     #
     def initialize(tname, schema, opts = {}, &on_before_row_added)
-      file_name = opts[:file_name]
-      unless File.readable?(file_name)
-        raise "Can't read CSV file '#{file_name}'"
-      end
       csv_opts = {}
       csv_opts[:headers] = (opts[:has_csv_header] == true) #(opts.delete(:has_csv_header) == true)
       unless csv_opts[:headers]
         raise "Current implementation only works with CSV files which inlcude a schema description in the first line"
       end
 
-      encoding =  opts[:encoding] #opts.delete(:encoding)
-      mode =  "rb"
-      mode << ":#{encoding}" if encoding
-      csv = CSV.open(file_name, mode, csv_opts)
+      if file_name = opts[:file_name]
+        unless File.readable?(file_name)
+          raise "Can't read CSV file '#{file_name}'"
+        end
+
+        encoding =  opts[:encoding] #opts.delete(:encoding)
+        mode =  "rb"
+        mode << ":#{encoding}" if encoding
+        idata = File.open(file_name, mode)
+      elsif idata = opts[:text]
+        # nothing else to do
+      else
+        raise "Don't know where to get content from."
+      end
+      csv = CSV.new(idata, csv_opts)
 
       unless schema = opts[:schema]
         unless csv_opts[:headers]
           raise "No schema given and ':has_csv_header' not set to capture schema from file header"
         end
-        first_row = csv.shift.fields # force reading the first row to have the header parsed
+        csv.shift # force reading the first row to have the header parsed
+        #puts ">>>>>> FIRST>>> #{first_row.inspect}"
         #csv.shift.each do |h, v| puts "#{h} => #{v.class}" end
         schema = csv.headers.map do |c|
           c = c.encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => '?')
           name, type = c.split(':')
           [name.strip, (type || 'string').strip]
         end
+        puts ">>>>>>>>>>>>>>>>>>>>>> SCHEMA: #{schema}"
+        csv.rewind # we had to grab that earlier to get schema
       end
       super tname, schema, opts
 
-      if first_row # from getting the CSV header
-        first_row.insert(0, @row_id += 1) if @add_index
-        @rows = [@schema.cast_row(first_row)]
-      end
+      # if first_row # from getting the CSV header
+      #   first_row.insert(0, @row_id += 1) if @add_index
+      #   @rows = [@schema.cast_row(first_row)]
+      # end
 
       # This assumes that CSV reader is setup with proper schema converters
       csv.each do |r|
